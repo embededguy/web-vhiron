e4e7688d3a1500af8f751091589819e159ae7846
MarkSpectre <114685111+MarkSpectre@users.noreply.github.com>
2023-07-08 14:43:00 +0530

    charging css loader added

---
+++ src/components/splash_new.js
@@ -0,0 +1,59 @@
+import './splash.css';
+
+function Splash(){
+	return(
+		<span class="loader"></span>
+		// <div className="animation-outer">
+		// <div className='animation-example'>
+		//     <div className='item'>
+		//         <div className='line'></div>
+		//         <div className='dot'></div>
+		//         <div className='circle'></div>
+		//     </div>
+		//     <div className='item'>
+		//         <div className='line'></div>
+		//         <div className='dot'></div>
+		//         <div className='circle'></div>
+		//     </div>
+		//     <div className='item'>
+		//         <div className='line'></div>
+		//         <div className='dot'></div>
+		//         <div className='circle'></div>
+		//     </div>
+		//     <div className='item'>
+		//         <div className='line'></div>
+		//         <div className='dot'></div>
+		//         <div className='circle'></div>
+		//     </div>
+		//     <div className='item -type2'>
+		//         <div className='line'></div>
+		//         <div className='dot'></div>
+		//         <div className='circle'></div>
+		//     </div>
+		//     <div className='item -type2'>
+		//         <div className='line'></div>
+		//         <div className='dot'></div>
+		//         <div className='circle'></div>
+		//     </div>
+		//     <div className='item -type2'>
+		//         <div className='line'></div>
+		//         <div className='dot'></div>
+		//         <div className='circle'></div>
+		//     </div>
+		//     <div className='item -type2'>
+		//         <div className='line'></div>
+		//         <div className='dot'></div>
+		//         <div className='circle'></div>
+		//     </div>
+		//     <div className='center'>
+		//         <div className='circle'></div>
+		//         <div className='circle'></div>
+		//         <div className='circle'></div>
+		//     </div>
+		// </div>
+		// </div>
+
+	);
+}
+
+export default Splash;
